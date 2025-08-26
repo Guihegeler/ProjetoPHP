605e6646b92c1586387ff23da17fb9976a68de99
Guihegeler <Guihegeler0@gmail.com>
2025-08-26 20:05:19 -0300

    Criando funções em PHP

--- funcoes.php
+++ funcoes.php
@@ -10,15 +10,24 @@
 
 		$resp = $a + $b;
 
-		echo("A soma de $a e $b é $resp");
+		return $resp;
+	}
+
+	function somaValores2($a, $b){
+
+		return ($a + $b);
 	}
 
 	$num1 = 10; $num2 = 20;
 
 	titulo();
-	somaValores($num1, $num2);
+	$resposta = somaValores($num1, $num2);
+	$resposta2 = somaValores2($num1, $num2);
 
+	$somando = somaValores(30, 40)+somaValores2(50, 60);
 
-
+	echo("A soma de $num1 e $num2 é $resposta <br />");
+	echo("A soma de $num1 e $num2 é $resposta2");
+	echo("<br /> $somando");
  ?>
 
